6dfc80d90e0eeb0bbb268093c5d49cc7caa05a6f
zahid mushtaq <mushtaqzahid888@gmail.com>
2023-03-06 21:31:35 +0530

    navbar and sidebar

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import { Movies, Actors, MovieInfo, Navbar, Profile } from './index';
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movies/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+export default App;
